f48fd4aa06d4b5e66d53d73480fce2a5a07181af
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-02 20:03:40 -0600

    Test: add test file and test for static image facade
    Co-authored-by: Logan Wilson <Loganwilson777@gmail.com>

---
+++ spec/facades/static_image_facade_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe StaticImageFacade do
+  describe 'instance methods' do
+    it "static_image" do
+      playground = StaticImageFacade.new("ChIJkTXfW-z1a4cRzrN-7OsfDNY").playground_image
+
+      expect(playground).to be_a(String)
+    end
+  end
+end
