e91e86a88a4e92e78ba6502ca0072ef786e0cbc0
Thomas Hawley <thawley22@turing.edu>
2023-06-04 12:55:59 -0600

    refactor: Move get_url and conn methods to private

--- app/services/playground_service.rb
+++ app/services/playground_service.rb
@@ -1,21 +1,22 @@
 class PlaygroundService
 
-  def get_url(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn
-    Faraday.new(url: "https://maps.googleapis.com/maps/api/place/") do |faraday|
-      faraday.params['key'] = ENV['GOOGLE_API_KEY']
-    end
-  end
-
   def get_playgrounds(location, radius)
     get_url("textsearch/json?query=playgrounds%20parks%20#{location}&radius=#{radius}")
   end
-
+  
   def get_playground(id)
     get_url("details/json?place_id=#{id}")
   end
+  
+  private
+    def get_url(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+    
+    def conn
+      Faraday.new(url: "https://maps.googleapis.com/maps/api/place/") do |faraday|
+        faraday.params['key'] = ENV['GOOGLE_API_KEY']
+      end
+    end
 end
