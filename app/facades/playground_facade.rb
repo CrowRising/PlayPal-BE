5e5740bad30b5913a7f1d423df66514aee494d1d
Crow Rising <miscbyrd@gmail.com>
2023-06-04 16:10:39 -0600

    feat: add image methods to playground

--- app/facades/playground_facade.rb
+++ app/facades/playground_facade.rb
@@ -8,14 +8,26 @@ class PlaygroundFacade
     @_playground ||= Playground.new(playground_data)
   end
 
+  def image
+    @_image ||= StaticImage.new(image_data)
+  end
+
   private
 
   def service
     @_service ||= PlaygroundService.new
   end
-
+  
   def playground_data
     @_playground_data ||= service.get_playground(@id)[:result]
+  end
+
+  def image_service
+    @_image_service ||= StaticImageService.new
+  end
+
+  def image_data
+    @_image_data ||= image_service.get_image(playground.lon, playground.lat)
   end
 
 end
