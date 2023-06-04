f676f9d8796cdb4c96805a1453bda0993d2a86e8
Crow Rising <miscbyrd@gmail.com>
2023-06-04 15:29:14 -0600

    test: add test for playground facade

--- spec/facades/playground_facade_spec.rb
+++ spec/facades/playground_facade_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe PlaygroundFacade do
   describe 'Playground Facade' do
-    it 'can create a single playground object' do
-      pg = PlaygroundFacade.new('ChIJz7xda7iLa4cRYNf2sN8C17I')
+    it 'can create a single playground object', :vcr do
+      pg = PlaygroundFacade.new('ChIJz7xda7iLa4cRYNf2sN8C17I').playground
+
+      expect(pg).to be_a Playground
+      expect(pg.id).to eq("ChIJz7xda7iLa4cRYNf2sN8C17I")
+      expect(pg.playground_name).to eq("Sensory Park")
+      expect(pg.playground_address).to eq("7577 W 103rd Ave, Westminster, CO 80021, USA")
+      expect(pg.rating).to eq(4.5)
     end
   end
 end
