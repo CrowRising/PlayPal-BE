4ebabb60e77e22088a647f19e38111826277fc48
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-03 23:01:49 -0600

    Build: add code for vcr gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,12 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['<GOOGLE_API_KEY>'] }
+  config.filter_sensitive_data('<MAPBOX_API_KEY>') { ENV['<MAPBOX_API_KEY>'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
