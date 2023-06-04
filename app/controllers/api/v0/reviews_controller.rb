b208cf52537163d7df296d871b38e88924a8fa33
Thomas Hawley <thawley22@turing.edu>
2023-06-04 17:44:32 -0600

    refactor: Update index and create action in the reviews controller

--- app/controllers/api/v0/reviews_controller.rb
+++ app/controllers/api/v0/reviews_controller.rb
@@ -1,20 +1,17 @@
 class Api::V0::ReviewsController < ApplicationController
   def index
-    @reviews = Review.all
-    render json: @reviews
+    @reviews = Review.where(playground_id: params[:playground_id])
+    render json: ReviewSerializer.new(@reviews)
   end
-
-  ## Will work on the create action later this is our rough draft
 
   def create
     @review = Review.create(review_params)
-    render json: @review
+    render json: 'Suppers ready and the street lights just came on!'
   end
 
-  
   private
 
-  def review_params
-    params.permit(:user_id, :playground_id, :rating, :comment, :image)
-  end
+    def review_params
+      params.require(:reviews).permit(:user_id, :playground_id, :rating, :comment, :image)
+    end
 end
