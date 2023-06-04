9ccc48308b1430a1b7989d2bafd8da01db84e1fb
Reilly Miller <reillyjmiller31@gmail.com>
2023-06-03 23:33:39 -0600

    Test: fix api key

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,8 +73,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['<GOOGLE_API_KEY>'] }
-  config.filter_sensitive_data('<MAPBOX_API_KEY>') { ENV['<MAPBOX_API_KEY>'] }
+  config.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
+  config.filter_sensitive_data('<MAPBOX_API_KEY>') { ENV['MAPBOX_API_KEY'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
