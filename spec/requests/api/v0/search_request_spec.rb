ff6d4738fb9009a3778df65c035f5601977a80e0
Thomas Hawley <thawley22@turing.edu>
2023-06-04 14:23:29 -0600

    test: Add test for playgrounds index action

--- spec/requests/api/v0/search_request_spec.rb
+++ spec/requests/api/v0/search_request_spec.rb
@@ -2,15 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'Search', type: :request do
   describe 'Playground search' do
-    xit 'returns all playgrounds' do
+    it 'returns all playgrounds', :vcr do
+     
+      headers = {"CONTENT_TYPE" => "application/json"}
+      get "/api/v0/playgrounds/80123/1", headers: headers
 
-      # get '/api/v0/playgrounds/39.7392/-104.9903/1000'
-      # formatted_address
-      # geometry[:location][:lat]
-      # geometry[:location][:lng]
-      # name
-      # place_id
-      # rating
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      playgrounds = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(playgrounds).to be_a Hash
+      expect(playgrounds).to have_key(:data)
+      expect(playgrounds[:data]).to be_an Array
+
+      playgrounds[:data].each do |pg|
+        expect(pg).to have_key(:id)
+        expect(pg).to have_key(:attributes)
+        expect(pg[:attributes]).to have_key(:playground_name)
+        expect(pg[:attributes]).to have_key(:playground_address)
+        expect(pg[:attributes]).to have_key(:rating)
+      end
     end
   end
 end
