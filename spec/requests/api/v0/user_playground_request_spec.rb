4afce7c3c21c884a365abd2227b12c31a8557627
Crow Rising <miscbyrd@gmail.com>
2023-06-04 20:15:12 -0600

    fix: user playgrounds controller index action

--- spec/requests/api/v0/user_playground_request_spec.rb
+++ spec/requests/api/v0/user_playground_request_spec.rb
@@ -5,21 +5,22 @@ RSpec.describe 'UserPlaygrounds', type: :request do
     it 'returns all user_playgrounds' do
       create_list(:user_playground, 10, user_id: 5)
 
-      get "/api/v0/users/5/favorites"
+      headers = {"CONTENT_TYPE" => "application/json"}
+      get "/api/v0/users/5/favorites", headers: headers
 
       expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body).size).to eq(10)
-
-      user_playground = JSON.parse(response.body)
-      user_playground.each do |user_playground|
-        expect(user_playground).to have_key('id')
-        expect(user_playground['id']).to be_a(Integer)
-        expect(user_playground).to have_key('user_id')
-        expect(user_playground['user_id']).to be_a(Integer)
-        expect(user_playground).to have_key('playground_id')
-        expect(user_playground['playground_id']).to be_a(Integer)
-        expect(user_playground).to have_key('playground_name')
-        expect(user_playground['playground_name']).to be_a(String)
+ 
+      user_playgrounds = JSON.parse(response.body, symbolize_names: true)
+# require 'pry'; binding.pry
+      user_playgrounds[:data].each do |user_playground|
+        expect(user_playground).to have_key(:id)
+        expect(user_playground[:id]).to be_a String
+        expect(user_playground[:attributes]).to have_key(:user_id)
+        expect(user_playground[:attributes][:user_id]).to be_an Integer
+        expect(user_playground[:attributes]).to have_key(:playground_id)
+        expect(user_playground[:attributes][:playground_id]).to be_an Integer
+        expect(user_playground[:attributes]).to have_key(:playground_name)
+        expect(user_playground[:attributes][:playground_name]).to be_a String
       end 
     end
   end
@@ -32,7 +33,7 @@ RSpec.describe 'UserPlaygrounds', type: :request do
         playground_name: 'This is a test playground name'
       })
       headers = { 'CONTENT_TYPE' => 'application/json' }
-      post '/api/v0/users/favorites', headers: headers, params: JSON.generate(user_playground_params)
+      post '/api/v0/users/favorites', headers: headers, params: JSON.generate(user_playgrounds: user_playground_params)
       created_user_playground = UserPlayground.last
       
       expect(response).to be_successful
