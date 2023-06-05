f3cd01f353fba4e8a8bfa81ee6685318f6c99786
Crow Rising <miscbyrd@gmail.com>
2023-06-04 20:14:25 -0600

    test: create working test for user playgrounds index

--- app/controllers/api/v0/user_playgrounds_controller.rb
+++ app/controllers/api/v0/user_playgrounds_controller.rb
@@ -1,17 +1,16 @@
 class Api::V0::UserPlaygroundsController < ApplicationController
     def index
-        @user_playgrounds = UserPlayground.all
-        render json: @user_playgrounds
+        @user_playgrounds = UserPlayground.where(user_id: params[:user_id])
+        render json: UserPlaygroundsSerializer.new(@user_playgrounds)
     end
 
     def create
         @user_playground = UserPlayground.create(user_playground_params)
-        render json: @user_playground
     end
 
     private
 
     def user_playground_params
-        params.require(:user_playground).permit(:user_id, :playground_id, :playground_name)
+        params.require(:user_playgrounds).permit(:user_id, :playground_id, :playground_name)
     end
 end
