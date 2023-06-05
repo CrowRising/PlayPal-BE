4b3475018afc21250831dbd97d81291f433d4185
Thomas Hawley <thawley22@turing.edu>
2023-06-05 15:49:25 -0600

    refactor: Remove static image spec and add image to tests

--- spec/poros/playground_spec.rb
+++ spec/poros/playground_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe Playground do
   describe "initialize" do
     it 'exists' do
-      data = {formatted_address: "450 Powers St, Erie, CO 80516, United States", name: "Lehigh Park", place_id: "ChIJkTXfW-z1a4cRzrN-7OsfDNY", rating: 4.6, image: 'image_url'}
-      playground = Playground.new(data)
+      data = {formatted_address: "450 Powers St, Erie, CO 80516, United States", name: "Lehigh Park", place_id: "ChIJkTXfW-z1a4cRzrN-7OsfDNY", rating: 4.6}
+      image_data = 'image_url'
+      playground = Playground.new(data, image_data)
 
       expect(playground).to be_a(Playground)
       expect(playground.playground_address).to eq("450 Powers St, Erie, CO 80516, United States")
