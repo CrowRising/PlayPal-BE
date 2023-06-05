c3e1e51c71873c3777a6b6b69c0ed386d5ee9109
Thomas Hawley <thawley22@turing.edu>
2023-06-05 15:33:09 -0600

    refactor: Update get_image method to use unsplash api

--- app/services/static_image_service.rb
+++ app/services/static_image_service.rb
@@ -1,25 +1,18 @@
 class StaticImageService
 
+  def get_image
+    get_url("/photos/random?query=kids,playground,park")
+  end
+  
+  private
   def get_url(url)
-    response = conn.get(url, headers: { 'Accept' => 'image/jpeg' })
-    jpeg_data = response.body
-
-    # @encoded_image_data = Base64.encode64(jpeg_data)
-    # file_path = Rails.root.join('app', 'image', 'image.jpg')
-
-    # File.open(file_path, 'wb') do |file|
-    #   file.write(jpeg_data)
-    # end
-   
+    response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/") do |faraday|
-      faraday.params['access_token'] = ENV['MAPBOX_API_KEY']
+    Faraday.new(url: "https://api.unsplash.com") do |faraday|
+      faraday.params['client_id'] = ENV['UNSPLASH_API_KEY']
     end
-  end
-
-  def get_image(lon, lat)
-    get_url("#{lon},#{lat},17,0,24/450x300")
   end
 end
